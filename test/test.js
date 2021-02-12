e4894669d937d76c2f99917d4541a64331a7dffc
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-12 17:46:18 +0100

    draft: notin much changd

--- test/test.js
+++ test/test.js
@@ -1,12 +1,9 @@
 var test = require("..")
 
-test("simple storage contract", async function (t) {
-  var data = 419
-  var contract = await t.contract("SimpleStorage", {/* ctor props */})
-
-  await contract.set(419)
-
-  var got = await contract.get()
-
-  t.same(data, got)
-})
+// test("simple storage contract", async function (t) {
+//   var data = 419
+//   var contract = await t.contract("SimpleStorage", {/* ctor props */})
+//   await contract.set(419)
+//   var got = await contract.get()
+//   t.same(data, got)
+// })
