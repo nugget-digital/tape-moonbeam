6d49adeb0634c49a19dab17d42463aadaf12595c
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-24 17:59:17 +0100

    test: passin da first real test case :D

--- test.js
+++ test.js
@@ -1,18 +1,22 @@
-const test = require('.')
+const test = require(".")
 
-test('alice transfers to bob', async t => {
-  const unsub = await t.api.tx.balances
-    .transfer(t.bob.address, 12345)
-    .signAndSend(t.alice, result => {
-      t.comment(`current status is ${result.status}`)
+test("alice transfers to bob", async t => {
+  const privKey =
+    "99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342" // Genesis private key
+  const addressFrom = "0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b"
+  const addressTo = "0x44236223aB4291b93EEd10E4B511B37a398DEE55" // Change addressTo
 
-      if (result.status.isInBlock) {
-        t.comment(`Transaction included at blockHash ${result.status.asInBlock}`)
-      } else if (result.status.isFinalized) {
-        t.comment(`Transaction finalized at blockHash ${result.status.asFinalized}`)
-        unsub()
-        t.pass()
-        t.end()
-      }
-    })
+  const tx = await t.web3.eth.accounts.signTransaction(
+    {
+      from: addressFrom,
+      to: addressTo,
+      value: t.web3.utils.toWei("100", "ether"),
+      gas: 21000
+    },
+    privKey
+  )
+
+  const receipt = await t.web3.eth.sendSignedTransaction(tx.rawTransaction)
+
+  t.ok(receipt.transactionHash)
 })
