23b57bd6bd23607698bb5dcf460d4fce54464d2e
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-25 12:28:27 +0100

    wip: switch from http 2 ipc 4 pubsub support

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var tape = require("tape")
 var Result = require("tape/lib/results")
 var moonbeam = require("./lib/moonbeam")
 
+var helpers = require('eth-helpers')
+var sodium = require('sodium-native')
+var ethKeygen = require('eth-keygen')
+var signer = require('eth-sign')
+var assert = require("nanoassert")
+
 let customTypes
 let exec
 
@@ -22,8 +28,8 @@ var _createStream = Result.prototype.createStream
 Result.prototype.createStream = function (...args) {
   var duplex = duplexify()
 
-  moonbeam(async web3 => {
-    tape.Test.prototype.web3 = web3
+  moonbeam(async eth => {
+    tape.Test.prototype.eth = eth
 
     duplex.setReadable(_createStream.call(this, ...args))
 
@@ -34,6 +40,81 @@ Result.prototype.createStream = function (...args) {
   }, { exec, customTypes }).catch(err => duplex.destroy(err))
 
   return duplex
+}
+
+var address = '0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b'
+var privateKey = Buffer.from('99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342', 'hex')
+
+tape.Test.prototype.fund = async function fund (to, value, data) {
+  assert(to != null, 'to must be given')
+  assert(value != null, 'value must be given')
+
+  var txHash = await this.send({
+    from: address,
+    to,
+    value: value,
+    gasPrice: 1,
+    gas: 8000000n,
+    data
+  }, privateKey)
+
+  return await this.mined(txHash)
+}
+
+tape.Test.prototype.mined = async function mined (tx) {
+  assert(tx != null, 'tx must be given')
+
+  // eslint-disable-next-line
+  return new Promise(async (resolve, reject) => {
+    const unlisten = await this.eth.subscribe(this.eth.getTransactionReceipt(tx), function (err, res) {
+      if (err) return
+
+      unlisten()
+      if (helpers.utils.parse.boolean(res.status) === true) return resolve(res)
+      return reject(new Error(res))
+    })
+  })
+}
+
+tape.Test.prototype.keygen = async function keygen (privateKey) {
+  return ethKeygen(privateKey, helpers.utils.parse.number(await this.eth.chainId()))
+}
+
+tape.Test.prototype.keygenSeed = async function keygenSeed (seed) {
+  assert(seed != null, 'seed must be given')
+
+  const privateKey = Buffer.alloc(32)
+  sodium.crypto_generichash(privateKey, Buffer.from(seed))
+
+  return ethKeygen(privateKey, helpers.utils.parse.number(await this.eth.chainId()))
+}
+
+tape.Test.prototype.sign = async function sign ({
+  from,
+  to,
+  value = 0,
+  data,
+  gas = 8e6,
+  gasPrice = 1,
+  nonce
+}, privateKey) {
+  assert(privateKey != null, 'privateKey must be given')
+
+  return signer.sign({
+    from: from == null ? undefined : helpers.utils.format(from),
+    to: to == null ? undefined : helpers.utils.format(to),
+    value: value == null ? undefined : helpers.utils.format(value),
+    data: data == null ? undefined : helpers.utils.format(data),
+    gas: helpers.utils.format(gas),
+    gasPrice: helpers.utils.format(gasPrice),
+    nonce: nonce != null ? helpers.utils.format(nonce) : await this.eth.getTransactionCount(helpers.utils.format(from), 'pending')
+  }, privateKey/*, helpers.utils.parse.number(await this.eth.chainId())*/)
+}
+
+tape.Test.prototype.send = async function send (txObj, privateKey) {
+  const tx = await this.sign(txObj, privateKey)
+
+  return this.eth.sendRawTransaction(helpers.utils.format(tx.raw))
 }
 
 module.exports = tape
--- test.js
+++ test.js
@@ -1,22 +1,48 @@
 var test = require(".")
 
-test("alice transfers to bob", async t => {
-  var privKey =
-    "99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342" // Genesis private key
-  var addressFrom = "0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b"
-  var addressTo = "0x44236223aB4291b93EEd10E4B511B37a398DEE55" // Change addressTo
-
-  var tx = await t.web3.eth.accounts.signTransaction(
-    {
-      from: addressFrom,
-      to: addressTo,
-      value: t.web3.utils.toWei("100", "ether"),
-      gas: 21000
-    },
-    privKey
-  )
-
-  var receipt = await t.web3.eth.sendSignedTransaction(tx.rawTransaction)
-
-  t.ok(receipt.transactionHash)
+test('chain id', async t => {
+  var chainId = await t.eth.chainId()
+  t.comment("chainId " + chainId)
+  t.ok(chainId)
+  t.end()
 })
+
+test('fund', async function (t) {
+  const acc = await t.keygen()
+  console.log("acc", acc)
+  await t.fund(acc.address, 10000)
+
+  t.equal(10000, parseInt(await t.eth.getBalance(acc.address)))
+
+  const tx = await t.sign({
+    from: acc.address,
+    to: '0x0000000000000000000000000000000000000000',
+    value: 10000
+  }, acc.privateKey)
+  console.log("tx", tx)
+  console.log(await t.trace(tx))
+
+  t.end()
+})
+
+// test("alice transfers to bob", async t => {
+//   var privKey =
+//     "99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342" // Genesis private key
+//   var addressFrom = "0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b"
+//   var addressTo = "0x44236223aB4291b93EEd10E4B511B37a398DEE55" // Change addressTo
+
+//   var tx = await t.web3.eth.accounts.signTransaction(
+//     {
+//       from: addressFrom,
+//       to: addressTo,
+//       value: t.web3.utils.toWei("100", "ether"),
+//       gas: 21000
+//     },
+//     privKey
+//   )
+
+//   var receipt = await t.web3.eth.sendSignedTransaction(tx.rawTransaction)
+
+//   t.ok(receipt.transactionHash)
+// })
+
