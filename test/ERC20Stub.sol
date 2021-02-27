b4bbad7865554fc6e5ae18f407fdf3ff1607e692
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-02-27 11:36:22 +0100

    impl: compile opts

--- test/ERC20Stub.sol
+++ test/ERC20Stub.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.2.0-solc-0.7/contracts/token/ERC20/ERC20.sol';
